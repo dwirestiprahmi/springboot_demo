4a2f9723f4452caae76487f1752932879a5b1cd4
Dwiresti Puspita Rahmi <dwiresti.p.rahmi@stud.h-da.de>
2022-08-26 10:00:04 +0200

    implemented the start of updateStudent

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+//all of the ressources for API, serves as the API Layer
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController //makes the class serve as a restful endpoint
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired //inject instantiation, dependency injection
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
+    @GetMapping //Get something out from the server (Restful endpoint)
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+}
